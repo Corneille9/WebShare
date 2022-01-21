5f27aac65f57004eba206a16acf166146b1002b5
Corneille <bankolecorneille@gmail.com>
2022-01-21 09:50:48 +0100

    cf

--- app/utils/utilities.py
+++ app/utils/utilities.py
@@ -1,23 +1,37 @@
 from pathlib import Path
 
 
-def get_file_icon(path):
+def get_file_icon(path, type_only=False):
     pos = path[::-1].find(".")
     if pos != -1:
         extension = path[-path[::-1].find("."):]
         if extension_box.get("images").__contains__(extension):
+            if type_only:
+                return "images"
             return icon_dict.get("images")
         elif extension_box.get("videos").__contains__(extension):
+            if type_only:
+                return "videos"
             return icon_dict.get("videos")
         elif extension_box.get("musics").__contains__(extension):
+            if type_only:
+                return "musics"
             return icon_dict.get("musics")
         elif extension_box.get("document").__contains__(extension):
+            if type_only:
+                return "document"
             return icon_dict.get("document")
         elif extension_box.get("word").__contains__(extension):
+            if type_only:
+                return "word"
             return icon_dict.get("word")
         elif extension_box.get("powerPoint").__contains__(extension):
+            if type_only:
+                return "powerPoint"
             return icon_dict.get("powerPoint")
         elif extension_box.get("archive").__contains__(extension):
+            if type_only:
+                return "archive"
             return icon_dict.get("archive")
 
     return icon_dict.get("unknown")
--- web/app.py
+++ web/app.py
@@ -3,6 +3,8 @@ import socket
 
 from quart import Quart, render_template, send_from_directory, jsonify, abort
 
+from app.utils.utilities import get_file_icon
+
 
 class SharedFiles:
     def __init__(self, mdapp):
@@ -17,9 +19,11 @@ class SharedFiles:
         @self.app.route('/')
         async def index():
             files = []
+            pos = 0
             for file in self.files:
                 if os.path.isfile(file):
-                    files.append(os.path.basename(file))
+                    files.append((os.path.basename(file), get_file_icon(file, type_only=True), pos, "200MB"))
+                    pos += 1
             return await render_template('index.html', files=files)
 
         @self.app.route('/isConnected')
@@ -47,6 +51,14 @@ class SharedFiles:
             for file in self.files:
                 if os.path.basename(file) == path:
                     return await send_from_directory(os.path.dirname(file), path, as_attachment=True)
+            abort(400, "No file allowed")
+
+        @self.app.route("/WebShare/video_feed/<path:path>")
+        async def video_feed(path):
+            for file in self.files:
+                if os.path.basename(file) == path:
+                    
+                    pass
             abort(400, "No file allowed")
 
     def run(self):
